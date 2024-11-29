27352b9e1ddfa1c24652d735636ba162f6b09d3d
samuel-c-santos <samuelsantosambiental@gmail.com>
2024-11-29 02:17:21 -0300

    Adicionando suporte para porta variável no Render

--- app.py
+++ app.py
@@ -126,4 +126,6 @@ def atualizar_graficos(regiao_selecionada, ano_selecionado):
 
 # Executar a aplicação
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=False, host='0.0.0.0', port=port)
